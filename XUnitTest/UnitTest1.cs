f37921c43b82121bfa52f6bf99cf879414c02e8d
KamranMirzeyev <kamran.mirzeyev@gts.az>
2022-10-26 11:43:53 +0400

    added test

---
+++ XUnitTest/UnitTest1.cs
@@ -0,0 +1,34 @@
+using System.Reflection;
+namespace XUnitTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+          Assert.Equal(4, Add(2, 2));
+            
+    }
+    [Fact]
+    public void FailingTest()
+    {
+        Assert.Equal(5, Add(2, 2));
+    }
+    int Add(int x, int y)
+    {
+        return x + y;
+    }
+    [Theory]
+    [InlineData(3)]
+    [InlineData(5)]
+    [InlineData(6)]
+    public void MyFirstTheory(int value)
+    {
+        Assert.True(IsOdd(value));
+    }
+
+    bool IsOdd(int value)
+    {
+        return value % 2 == 1;
+    }
+}
